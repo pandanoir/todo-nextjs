d218067508b828de5a5b99e0a0c02d086b2aaffd
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-25 13:48:48 +0900

    refactor

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,9 +8,7 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
+  global.prisma ??= new PrismaClient();
   prisma = global.prisma;
 }
 
