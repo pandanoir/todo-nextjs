0e13cd3fd7e92e215c83d58d5c66b1d037cea915
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-26 19:04:12 +0900

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Top page</h1>
-        <Link href="/todo">app page</Link>
-        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a href="/api/auth/login">login</a>
-      </main>
-    </div>
-  );
-}
+const TopPage = () => (
+  <div className={styles.container}>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Top page</h1>
+      <Link href="/todo">app page</Link>
+      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+      <a href="/api/auth/login">login</a>
+    </main>
+  </div>
+);
+export default TopPage;
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,67 +1,69 @@
 'use client';
 import { UserProfile, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
-import Link from 'next/link';
 import { FC } from 'react';
 import useSWR, { useSWRConfig } from 'swr';
 import styles from '../../styles/Home.module.css';
 
 type Todo = { title: string; done: boolean; id: string };
 const useTodo = () => {
+  const { mutate } = useSWRConfig();
   const { data: todos, error } = useSWR<Todo[]>('/api/todo/read', (url) =>
     fetch(url).then((res) => res.json())
   );
-  const { mutate } = useSWRConfig();
+
   if (!todos) {
     return { todos } as const;
   }
-  const update = (id: string, newTodo: Todo) => {
-    const index = todos.findIndex((x) => x.id === id);
-    mutate(
-      '/api/todo/read',
-      (async () => [
-        ...todos.slice(0, index),
-        await fetch('/api/todo/update', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(newTodo),
-        }).then((res) => res.json()),
-        ...todos.slice(index + 1),
-      ])(),
-      {
-        optimisticData: [
+  return {
+    todos,
+    error,
+    createNewTodo: () => {
+      mutate(
+        '/api/todo/read',
+        (async () => [
+          await fetch('/api/todo/create').then((res) => res.json()),
+          ...todos,
+        ])(),
+        {
+          optimisticData: [{ id: 'temp', title: '', done: false }, ...todos],
+          rollbackOnError: true,
+        }
+      );
+    },
+    update: (id: string, newTodo: Todo) => {
+      const index = todos.findIndex((x) => x.id === id);
+      mutate(
+        '/api/todo/read',
+        (async () => [
           ...todos.slice(0, index),
-          newTodo,
+          await fetch('/api/todo/update', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newTodo),
+          }).then((res) => res.json()),
           ...todos.slice(index + 1),
-        ],
-        rollbackOnError: true,
-      }
-    );
-  };
-  const createNewTodo = () => {
-    mutate(
-      '/api/todo/read',
-      (async () => [
-        await fetch('/api/todo/create').then((res) => res.json()),
-        ...todos,
-      ])(),
-      {
-        optimisticData: [{ id: 'temp', title: '', done: false }, ...todos],
-        rollbackOnError: true,
-      }
-    );
-  };
-  return { todos, error, update, createNewTodo } as const;
+        ])(),
+        {
+          optimisticData: [
+            ...todos.slice(0, index),
+            newTodo,
+            ...todos.slice(index + 1),
+          ],
+          rollbackOnError: true,
+        }
+      );
+    },
+  } as const;
 };
 
 const TodoPage: FC<{ user: UserProfile }> = () => {
   const { todos, update, createNewTodo } = useTodo();
-  console.log(todos, styles);
   return (
     <div className={styles.container}>
-      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-      <a href="/api/auth/logout">Logout</a>
       <main className={styles.main}>
         <h1 className={styles.title}>Todo sample</h1>
+        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+        <a href="/api/auth/logout">Logout</a>
 
         <p className={styles.description}>
           todo list <button onClick={createNewTodo}>add</button>
