c46e09f168f8ea8bcfd9e11325168f7be8616d2e
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-26 18:36:45 +0900

    change redirect URL on login

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,2 +1,4 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
-export default handleAuth();
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+export default handleAuth({
+  login: (req, res) => handleLogin(req, res, { returnTo: '/todo' }),
+});
